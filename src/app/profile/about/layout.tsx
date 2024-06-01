0b94fce5b3e5cb0c40a3f67b16814b2a47ff0c1c
vann19 <ahmadfansya660@gmail.com>
2024-06-01 21:35:29 +0700

    next js start

---
+++ src/app/profile/about/layout.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage({ children }: { children: React.ReactNode }) {
+    return (
+      <>
+      <h1>Title</h1>
+      <div>{children}</div>
+      </>
+    );
+  }
---
+++ src/app/profile/about/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AboutPage() {
+    return (
+      <div>
+        <h1>ini halaman about</h1>
+      </div>
+    );
+  }
