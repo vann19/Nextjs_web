d8103985ed248eb2d8ec4aaefdd921790d8d7213
vann19 <ahmadfansya660@gmail.com>
2024-06-02 00:04:33 +0700

    Router Group & Router Dynamic

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Halaman Login</h1>
+    </div>
+  );
+}
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { useState } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  
+  const [state, setstate] = useState(0);
   return (
-    <div>{children}</div>
+    <div>
+      {/* <div>Hello world {state}</div> */}
+      {/* <button onClick={() => setstate(state + 1)}>+</button> */}
+      {children}
+    </div>
   );
 }
