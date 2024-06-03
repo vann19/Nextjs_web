81c751933e2c5c35876980e1727e42e10ead17ce
vann19 <ahmadfansya660@gmail.com>
2024-06-03 23:52:33 +0700

    Data Fetching

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import { title } from "process";
 
 const data = [
   {
     id: 1,
-    name: "Sepatu Baru",
+    title: "Sepatu Baru",
     Price: 100000,
+    image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/32090ca9-d8ef-4943-aa41-4f23dc0777af/air-jordan-4-retro-oxidised-green-shoes.png"  ,
   },
   {
     id: 2,
-    name: "Baju Baru",
+    title: "Sepatu Jordan",
     Price: 50000,
+    image:  "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/32090ca9-d8ef-4943-aa41-4f23dc0777af/air-jordan-4-retro-oxidised-green-shoes.png" ,
   },
 ];
 
