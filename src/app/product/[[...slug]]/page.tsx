b66c40d20ac1bae4f75f5741470789d046654a3c
vann19 <ahmadfansya660@gmail.com>
2024-06-09 18:40:27 +0700

    Loading UI & Error Handling

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,10 +1,12 @@
 type ProductPageProps = { params: { slug: string[] } };
 
 async function getData() {
-  // const res = await fetch("https://fakestoreapi.com/products");
-  const res = await fetch("http://localhost:3000/api/product", {
-    // cache: "no-store",
-    cache: "force-cache",
+  // const res = await fetch("https://fakestoreapi.com/products", {
+  //   cache: "no-store",
+  // });
+  const res = await fetch("http://localhost:3000/api/productfdfdf", { //perbaikinn kalau mau lihat ui error nya
+    cache: "no-store",
+    // cache: "force-cache",
     next: {
       tags: ["products"],
       // revalidate: 10,
