e2a678e32b68de81f9a33533162330257543e161
vann19 <ahmadfansya660@gmail.com>
2024-06-11 22:58:26 +0700

    Intercepting Route

---
+++ src/app/product/@modal/(.)detail/[id]/page.tsx
@@ -0,0 +1,21 @@
+import Modal from "@/components/core/Modal";
+import { getData } from "@/services/products";
+import { data } from "autoprefixer";
+
+export default async function DetailProductPage(props: any) {
+  const { params } = props;
+  const product = await getData("http://localhost:3000/api/product/?id=" + params.id);
+  return (
+    <Modal>
+      <img
+       src={product.data.image} 
+       alt="" 
+       className="w-full object-cover aspect-square col-span-2" 
+       />
+      <div className="bg-white p-4 px-6">
+        <h3>{product.data.title}</h3>
+        <p>Price : ${product.data.price}</p>
+      </div>
+    </Modal>
+  );
+}
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,27 +1,12 @@
+import { getData } from "@/services/products";
+import Link from "next/link";
+
 type ProductPageProps = { params: { slug: string[] } };
 
-async function getData() {
-  // const res = await fetch("https://fakestoreapi.com/products", {
-  //   cache: "no-store",
-  // });
-  const res = await fetch("http://localhost:3000/api/product", { 
-    cache: "no-store",
-    // cache: "force-cache",
-    next: {
-      tags: ["products"],
-      // revalidate: 10,
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
 
 export default async function ProductPage(props: ProductPageProps) {
   const { params } = props;
-  const products = await getData();
+  const products = await getData("http://localhost:3000/api/product");
   console.log(products);
   console.log(params);
   return (
@@ -29,26 +14,26 @@ export default async function ProductPage(props: ProductPageProps) {
       {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
       {products.data.length > 0 &&
         products.data.map((products: any) => (
-          <div key={products.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
-            <a href="#">
+          <Link
+           href={`/product/detail/${products.id}`}
+           key={products.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
+
               <img className="p-8 rounded-t-lg object:cover h-96 w-full" src={products.image} alt="product image" />
-            </a>
             <div className="px-5 pb-5">
-              <a href="#">
+
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">{products.title}</h5>
-              </a>
 
               <div className="flex items-center justify-between mt-3">
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">$ {products.price}</span>
-                <a
-                  href="#"
+                <button
+                  type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Add to cart
-                </a>
+                </button>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       {params.slug && (
         <>
